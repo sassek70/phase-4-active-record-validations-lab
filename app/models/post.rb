456e9a9f3c059d92bccf86dfbbef539e72c2a311
Kevin Sasse <sassek70@gmail.com>
2022-11-23 11:16:13 -0500

    commit tree 474f2e25cc2b12351e5f0bbcafb2571c1418a126

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+
+    #options for checking if a key includes something.
+        #using in: Array
+        validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+        #using % notation - %w() turns items in parenthesis into strings in an array - short hand
+        validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    validates :title, exclusion: { in: ["True Facts"]}
+
+
 end
